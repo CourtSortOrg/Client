59ac1121861b1a2e60cb0517496f354925e5eab0
Daniel Kambich <dkambich@purdue.edu>
2019-02-06 09:51:23 -0500

    Added a button for testing purposes
    Setting groundwork to allow navigation between 2 screens so we can develop multiple screens in parallel

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app</Text>
+        <Button
+          onPress={() => {Alert.alert('You tapped the button!');}}
+          title="Press Me"
+        />
       </View>
     );
   }
