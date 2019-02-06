73807b492e145287c3d5762234b3c9f8eb3fdb47
William Eiffert <weiffert@gmail.com>
2019-02-06 10:23:26 -0500

    Add tab navigation to main screen

--- components/main/Main.js
+++ components/main/Main.js
@@ -1,12 +1,45 @@
 import React from "react";
 import {View, Text} from "react-native";
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
-export default class Main extends React.Component {
+import Home from "./Home";
+import Profile from "./Profile";
+import Notifications from "./Notifications";
+import Meals from "./Meals";
+import Messages from "./Messages";
+
+const StackNavigator = createStackNavigator({
+  Home: {
+    screen: Home
+  },
+  Profile: {
+    screen: Profile
+  },
+  Notifications: {
+    screen: Notifications
+  }
+});
+
+const TabNavigator = createBottomTabNavigator({
+  Home: {
+    screen: Home
+  },
+  Meals: {
+    screen: Meals
+  },
+  Messages: {
+    screen: Messages
+  }
+});
+
+const StackContainer = createAppContainer(StackNavigator);
+const TabContainer = createAppContainer(TabNavigator);
+
+export default TabContainer;
+/*export default class Main extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Main</Text>
-      </View>
+        <TabNavigator/>
     )
   }
-}
+}*/
