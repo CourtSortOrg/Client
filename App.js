97f2c44cb35e8b2b6eb73afcbc608976e44a60e3
William Eiffert <weiffert@gmail.com>
2019-02-06 10:22:16 -0500

    Add switch navigation for authentication

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createSwitchNavigator, createAppContainer } from "react-navigation"
 
-export default class App extends React.Component {
+import Main from "./components/main/Main";
+import Splash from "./components/auth/Splash"
+import SignIn from "./components/auth/SignIn"
+
+const AuthNavigation = createSwitchNavigator(
+  {
+    Splash: {
+      screen: Splash
+    },
+    Home: {
+      screen: Main
+    },
+    Auth: {
+      screen: SignIn
+    },
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const App = createAppContainer(AuthNavigation);
+export default App;
+
+/*export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <AuthContainer />
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
-}
+}*/
 
 const styles = StyleSheet.create({
   container: {
