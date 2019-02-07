51405c066c0835d1fa37564b04286a590efc2359
William Eiffert <weiffert@gmail.com>
2019-02-07 17:42:22 -0500

    Skeleton code for showing a meal item's components.

--- components/main/MealItem.js
+++ components/main/MealItem.js
@@ -7,11 +7,31 @@ export default class MealItem extends React.Component {
   render() {
     return (
       <View>
-        <Text>MealItem</Text>
-        <Button
-          title="Go to dining court"
-          onPress={() => this.props.navigation.push("DiningCourt")}
-        />
+      <Text>{this.props.Name}</Text>
+      // Flex flow row. Just a box.
+      <View>
+      {
+        this.props.Allergens
+        .filter(allergen => allergen.Value)
+        .map(allergen => <AllergenIcon {...allergen} />)
+      }
+      </View>
+
+      // Start with a list.
+      <View>
+      {
+        this.props.Nutrition.map(fact => <NutritionFact {...fact})
+      }
+      </View>
+      
+      // Just a list.
+      <View>
+      <Text>{this.props.Incredients}</Text>
+      </View>
+      <Button
+      title="Go to dining court"
+      onPress={() => this.props.navigation.push("DiningCourt")}
+      />
       </View>
     )
   }
