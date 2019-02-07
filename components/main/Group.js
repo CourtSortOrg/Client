db56c588503f16ee8ee84494a3e858385c08cfd2
William Eiffert <weiffert@gmail.com>
2019-02-07 08:31:16 -0500

    Add more basic components

---
+++ components/main/Group.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+
+export default class Group extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Group</Text>
+      </View>
+    )
+  }
+}
