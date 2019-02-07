5d8802f65c84a91017393b97888fd817b698240c
William Eiffert <weiffert@gmail.com>
2019-02-07 08:37:50 -0500

    Add navigation structure. Not tested yet.

--- components/main/Main.js
+++ components/main/Main.js
@@ -2,44 +2,116 @@ import React from "react";
 import {View, Text} from "react-native";
 import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
+import DiningCourt from "./DiningCourt";
+import Friend from "./Friend";
+import Group from "./Group";
 import Home from "./Home";
-import Profile from "./Profile";
-import Notifications from "./Notifications";
+//import Map from "./Map";
+import MealItem from "./MealItem";
 import Meals from "./Meals";
+import Message from "./Message";
 import Messages from "./Messages";
+import Notifications from "./Notifications";
+import Profile from "./Profile";
 
-const StackNavigator = createStackNavigator({
+/*
+ * For navigation, the paths need to be absolutely defined.
+ * A navigator exists for everything.
+*/
+
+const SocialStack = createStackNavigator({
+  Friend: {
+    screen: Friend
+  },
+  Group: {
+    screen: Group
+  }
+});
+
+const ProfileStack = createStackNavigator({
+  Profile: {
+    screen: Profile
+  },
+  Social: {
+    screen: SocialStack
+  }
+});
+
+const MessageStack = createStackNavigator({
+  Message: {
+    screen: Message
+  },
+  Social: {
+    screen: SocialStack
+  }
+})
+
+const DiningDetailsStack = createStackNavigator({
+  MealItem: {
+    screen: MealItem
+  },
+  DiningCourt: {
+    screen: DiningCourt
+  },
+});
+
+const HomeStack = createStackNavigator({
   Home: {
     screen: Home
   },
   Profile: {
-    screen: Profile
+    screen: ProfileStack
   },
   Notifications: {
     screen: Notifications
+  },
+  DiningDetails: {
+    screen: DiningDetailsStack
   }
 });
 
-const TabNavigator = createBottomTabNavigator({
-  Home: {
-    screen: Home
-  },
+const MealsStack = createStackNavigator({
   Meals: {
     screen: Meals
   },
+  Profile: {
+    screen: ProfileStack
+  },
+  Notifications: {
+    screen: Notifications
+  },
+  DiningDetails: {
+    screen: DiningDetailsStack
+  },
+});
+
+const MessagesStack = createStackNavigator({
   Messages: {
     screen: Messages
+  },
+  Profile: {
+    screen: ProfileStack
+  },
+  Notifications: {
+    screen: Notifications
+  },
+  Message: {
+    screen: MessageStack
+  }
+})
+
+const Tab = createBottomTabNavigator({
+  Home: {
+    screen: HomeStack
+  },
+  Meals: {
+    screen: MealsStack
+  },
+  Messages: {
+    screen: MessagesStack
   }
 });
 
-const StackContainer = createAppContainer(StackNavigator);
-const TabContainer = createAppContainer(TabNavigator);
+const TabContainer = createAppContainer(Tab);
 
 export default TabContainer;
-/*export default class Main extends React.Component {
-  render() {
-    return (
-        <TabNavigator/>
-    )
-  }
-}*/
