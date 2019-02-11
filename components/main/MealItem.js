e9fbbd8a86942733e8a6008de7167459129e2bc9
William Eiffert <weiffert@gmail.com>
2019-02-11 16:34:14 -0500

    Fix syntatical errors. Page is broken due to improper passing of props.

--- components/main/MealItem.js
+++ components/main/MealItem.js
@@ -3,32 +3,33 @@ import { View, Text } from "react-native";
 import { Button } from "react-native-elements";
 
 import Screen from "../Nav/Screen";
+import AllergenIcon from "./AllergenIcon";
+import NutritionFact from "./NutitionFact";
 
 export default class MealItem extends React.Component {
   render() {
     return (
-      <View>
-      <Text>{this.props.Name}</Text>
-      // Flex flow row. Just a box.
-      <View>
-      {
-        this.props.Allergens
-        .filter(allergen => allergen.Value)
-        .map(allergen => <AllergenIcon {...allergen} />)
-      }
-      </View>
-
-      // Start with a list.
-      <View>
-      {
-        this.props.Nutrition.map(fact => <NutritionFact {...fact} />)
-      }
-      </View>
-
-      // Just a list.
-      <View>
-        <Text>{this.props.Ingredients}</Text>
-      </View>
+      <Screen
+        title={this.props.name}
+        navigation={{ ...this.props.navigation }}
+        backButton={true}
+      >
+        // Flex flow row. Just a box.
+        <View>
+          {this.props.Allergens &&
+            this.props.Allergens.filter(allergen => allergen.Value).map(
+              allergen => <AllergenIcon {...allergen} />
+            )}
+        </View>
+        // Start with a list.
+        <View>
+          {this.props.Nutrition &&
+            this.props.Nutrition.map(fact => <NutritionFact {...fact} />)}
+        </View>
+        // Just a list.
+        <View>
+          <Text>{this.props.Ingredients}</Text>
+        </View>
         <Button
           title="Go to dining court"
           onPress={() => this.props.navigation.push("DiningCourt")}
