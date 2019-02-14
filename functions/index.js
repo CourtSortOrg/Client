730834a4164b9909793f4ad86201b6ffce4786fa
ccirrin <ccirrin@gmail.com>
2019-02-13 23:36:17 -0500

    attempting to get the updateUserDatabase method to check if a user exists first before writing to the database

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,14 @@ const functions = require('firebase-functions');
 //var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const fetch = require("node-fetch");
 const req = require('request');
-const algorithms = require('./algorithms.js')
+//const algorithms = require('./algorithms.js')
+
+const admin = require('firebase-admin');
+const firebase = require('firebase');
+admin.initializeApp(functions.config().firebase);
+firebase.initializeApp(functions.config().firebase);
+
+var db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -30,4 +37,50 @@ exports.getMenus = functions.https.onRequest((request, response) => {
     resp.send(body);
   }
   requestMenu(processResult, response);
+});
+
+//weewoo get the user data and put in the database
+exports.updateUserDatabase = functions.https.onRequest((request, response) => {
+  function listAllUsers(nextPageToken, resp) {
+  // List batch of users, 1000 at a time.
+  admin.auth().listUsers(1000, nextPageToken)
+    .then(function(listUsersResult) {
+      listUsersResult.users.forEach(function(userRecord) {
+        //console.log("user", userRecord.toJSON());
+        userIntoDatabase(userRecord);
+      });
+      if (listUsersResult.pageToken) {
+        // List next batch of users.
+        listAllUsers(listUsersResult.pageToken)
+      }
+    })
+    .catch(function(error) {
+      console.log("Error listing users:", error);
+    });
+    resp.send("done");
+  }
+
+  //insert user into database
+  function userIntoDatabase(userRecord) {
+    if (!checkUserExists(userRecord.uid)) {
+      var updatedUser = {
+        uid: userRecord.uid,
+        preferences: "hi",
+        dietaryRestrictions: "",
+        friends: "",
+        blockedUsers: ""
+      }
+      db.collection("User").doc(userRecord.uid).set(updatedUser);
+    }
+  }
+
+  //check if the user exists already
+  function checkUserExists(uid) {
+    firebase.database().ref('/User/' + uid).once('value').then(function(snapshot) {
+      return snapshot.exists();
+    });
+  }
+
+  // Start listing users from the beginning, 1000 at a time.
+  listAllUsers(undefined, response);
 });
