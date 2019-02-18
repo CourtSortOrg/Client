7bef0f6f0bb45a2363f8ba86307deeb957307a42
Daniel Kambich <dkambich@purdue.edu>
2019-02-18 18:06:39 -0500

    Add Carousel to Map.js with sample data
    Added Carousel to the Map screen that will let you swipe cards to view a different dining court location

--- components/main/Map.js
+++ components/main/Map.js
@@ -1,14 +1,37 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
 import { MapView } from "expo";
+import Carousel from "react-native-snap-carousel";
 
-import Screen from "../Nav/Screen";
 import Header from "../Nav/Header";
 import Footer from "../Nav/Footer";
-
+import Card from "../Nav/Card";
+import Text from "../Nav/Text";
 
 export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+
+    var diningData = require("../../testData/diningCourtData.json");
+
+    this.state = {
+      diningCourts: diningData.courts
+    };
+  }
+
+  renderDiningCard = ({ item, index }) => {
+    return (
+      <Card header={item.name}>
+        <Text>This is where data will go</Text>
+        <Text>This is where data will go</Text>
+        <Text>This is where data will go</Text>
+        <Text>This is where data will go</Text>
+      </Card>
+    );
+  };
+
   render() {
+    var { height, width } = Dimensions.get("window");
     return (
       <View style={{ flex: 1 }}>
         <Header
@@ -17,19 +40,30 @@ export default class Map extends React.Component {
           navigation={{ ...this.props.navigation }}
           backButton={true}
         />
-        <MapView
-          style={{flex: 1, flexGrow: 1}}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          }}
-        />
-        <Footer
-        styles={styles}
-        navigation={{ ...this.props.navigation }}
-      />
+        <View style={{ flex: 1, flexGrow: 1 }}>
+          <MapView
+            style={{ flex: 1 }}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421
+            }}
+          />
+          <View style={{ position: "absolute", bottom: 25 }}>
+            <Carousel
+              ref={c => {
+                this._carousel = c;
+              }}
+              data={this.state.diningCourts}
+              renderItem={this.renderDiningCard}
+              sliderWidth={width}
+              itemWidth={width * 0.75}
+            />
+          </View>
+        </View>
+
+        <Footer styles={styles} navigation={{ ...this.props.navigation }} />
       </View>
     );
   }
