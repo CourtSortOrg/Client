bd6adc0d0b466a7339d4fec846a988af983c2666
Aahan Aggarwal <aahan.aggarwal99@gmail.com>
2019-02-18 21:04:51 -0500

    can now populate and retrieve menu dishes

--- functions/index.js
+++ functions/index.js
@@ -13,6 +13,110 @@ exports.test = functions.https.onRequest((request, response) => {
   response.send("Heyo!");
 });
 
+// this function returns a JSON of all dishes given a location and date
+// PARAMETERS: location, date (as a string)
+exports.fetchDishes = functions.https.onRequest(async (request, response)=> {
+	var location = request.body.location;
+  var date = request.body.date;
+
+  if(location == null || date == null){
+    // sets as default for testing
+    location = "hillenbrand";
+    date = "2019-02-18";
+	}
+	
+	console.log("Querying data");
+	var collectionRef = await db.collection("Dish");
+	var setOfDishes = await collectionRef.get().where("diningHall", "==", location).where("date", "array-contains", date).then(
+		querySnapshot => {
+			if(querySnapshot.empty){
+				response.send({error: "no matches", size: querySnapshot.size});
+			}else{
+				console.log("size: "+querySnapshot.size);
+				var items = [];
+				querySnapshot.forEach(doc => {
+					items.push(doc.data());
+				})
+				response.send(items);
+			}
+		}
+	).catch(err => {
+    console.log('Error getting documents', err);
+  });
+});
+
+// this function populates the database with new dishes
+// PARAMETERS: location, date (as a string)
+exports.populateDishes = functions.https.onRequest(async (request, response)=>{
+  var location = request.body.location;
+  var date = request.body.date;
+
+  if(location == null || date == null){
+    // sets as default for testing
+    location = "hillenbrand";
+    date = "2019-02-18";
+  }
+
+  const url = "https://api.hfs.purdue.edu/menus/v2/locations/" + location + "/" + date;
+  const getData = async url => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
+      return json;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  async function updateDatabase(data) {
+    var menuJSON = data;
+    console.log(JSON.stringify(menuJSON));
+
+    // sets basic info for hall for now such as menu items for particular meal
+    // ideally we would pass in the meal (lunch or dinner, etc), and this would then fill in the database
+    var hall = menuJSON['Location'];
+    for(var k = 0; k<menuJSON['Meals'].length; k++){
+      var mealInfo = menuJSON['Meals'][k];
+
+      // add every item for every station for particular meal
+      // also adds every meal to meal Collection if it exists
+      for(var i=0; i < mealInfo['Stations'].length; i++){
+        var currStation = mealInfo['Stations'][i];
+        
+        for(var j=0; j<currStation['Items'].length; j++){
+          var item = {
+            name: currStation['Items'][j]['Name'],
+            diningHall: location,
+            station: currStation['Name'],
+						dates: [date],
+						meal: k
+          };
+
+          var itemRef = db.collection('Dish').doc(item['name']);
+          var getItem = await itemRef.get().then(async doc => {
+            if (!doc.exists) {
+              itemRef.set(item);   
+              console.log("SET new item: "+item['name']);     
+            } else {
+							console.log("Modify: "+item['name']);
+							var arrayUnion = await itemRef.update({ dates: admin.firestore.FieldValue.arrayUnion(date)});
+							console.log("modified");
+            }
+          }).catch(err => {
+            console.log('Error getting document', err);
+          });
+        }
+      }
+    }
+  }
+  
+  var data = await getData(url);
+  var updated = await updateDatabase(data);
+  console.log("done");
+  response.send("Finished Population for "+location+" on "+date);
+});
+
 //simple function to get menus from dining court
 //PARAMETERS: none
 exports.getMenus = functions.https.onRequest((request, response) => {
