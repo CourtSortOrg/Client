6f48e0cfc0476bee69d4a78c41d28b1f56fd1070
Daniel Kambich <dkambich@purdue.edu>
2019-02-27 10:08:53 -0500

    Minor changes to formatting in Settings.js

--- components/Settings/Settings.js
+++ components/Settings/Settings.js
@@ -16,36 +16,39 @@ export default class Settings extends React.Component {
       >
         <ListItem
           title="Edit Profile"
+          subtitle="Make changes to your profile"
+          leftIcon={<Icon name="edit" />}
           onPress={() => {
             this.props.navigation.navigate("EditProfile");
           }}
-          subtitle="Make changes to your profile"
           topDivider
           bottomDivider
           chevron
-          leftIcon={<Icon name="edit" />}
         />
         <ListItem
           title="Blocked Users"
           subtitle="View and edit your blocked users"
+          leftIcon={<Icon name="block" />}
+          onPress={() => {}}
           topDivider
           bottomDivider
           chevron
-          leftIcon={<Icon name="block" />}
         />
         <ListItem
           title="Sign Out"
           subtitle="Sign out of your account"
+          leftIcon={<Icon name="log-out" type="feather" />}
+          onPress={() => {}}
           topDivider
           bottomDivider
-          leftIcon={<Icon name="log-out" type="feather" />}
         />
         <ListItem
           title="Delete Account"
           subtitle="Delete your account and all your data"
+          leftIcon={<Icon name="delete-forever" />}
+          onPress={() => {}}
           topDivider
           bottomDivider
-          leftIcon={<Icon name="delete-forever" />}
         />
       </Screen>
     );
