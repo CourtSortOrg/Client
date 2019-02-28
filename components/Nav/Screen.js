a720f5bf2e40136076750dedb697c2a38cc1d2e5
William Eiffert <weiffert@gmail.com>
2019-02-27 21:31:44 -0500

    Fix screen show no header.

--- components/Nav/Screen.js
+++ components/Nav/Screen.js
@@ -7,32 +7,37 @@ import Body from "./Body";
 
 export default class Screen extends React.Component {
   render() {
+    if (this.props.showNavigation == false) {
+      return (
+        <View
+          style={{
+            flex: 1
+          }}
+        >
+          <Body>{this.props.children}</Body>
+        </View>
+      );
+    }
     return (
       <View
         style={{
           flex: 1
         }}
       >
-        {this.props.showNavigation != true ? (
-          <Body>{this.props.children}</Body>
-        ) : (
-          <View>
-            <Header
-              styles={styles}
-              navigation={{ ...this.props.navigation }}
-              title={this.props.title}
-              backButton={this.props.backButton}
-              active={this.props.title}
-              {...this.props.header}
-            />
-            <Body>{this.props.children}</Body>
-            <Footer
-              styles={styles}
-              navigation={{ ...this.props.navigation }}
-              active={this.props.title}
-            />
-          </View>
-        )}
+        <Header
+          styles={styles}
+          navigation={{ ...this.props.navigation }}
+          title={this.props.title}
+          backButton={this.props.backButton}
+          active={this.props.title}
+          {...this.props.header}
+        />
+        <Body>{this.props.children}</Body>
+        <Footer
+          styles={styles}
+          navigation={{ ...this.props.navigation }}
+          active={this.props.title}
+        />
       </View>
     );
   }
