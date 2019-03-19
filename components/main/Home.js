80a4af3a483d707c9266f5305549a2a87695ef27
William Eiffert <weiffert@gmail.com>
2019-03-19 14:53:30 -0400

    Make a basic checkin interface on the home screen.

--- components/main/Home.js
+++ components/main/Home.js
@@ -4,6 +4,7 @@ import { Button } from "react-native-elements";
 
 import Screen from "../Nav/Screen";
 import CheckInCard from "./CheckInCard";
+import Card from "../components/Card";
 
 export default class Home extends React.Component {
   render() {
@@ -13,11 +14,59 @@ export default class Home extends React.Component {
         navigation={this.props.navigation}
         backButton={false}
       >
-        {this.props.screenProps.user.checkInLocation != undefined && (
+        {this.props.screenProps.user.checkInLocation != undefined ? (
           <CheckInCard
             navigation={this.props.navigation}
             screenProps={this.props.screenProps}
           />
+        ) : (
+          <Card header="Check into a Dining Court!">
+            <Card
+              footer={[
+                {
+                  text: "Hillenbrand",
+                  onPress: () =>
+                    this.props.screenProps.functions.checkIn("Hillenbrand")
+                }
+              ]}
+            />
+            <Card
+              footer={[
+                {
+                  text: "Ford",
+                  onPress: () =>
+                    this.props.screenProps.functions.checkIn("Ford")
+                }
+              ]}
+            />
+            <Card
+              footer={[
+                {
+                  text: "Wiley",
+                  onPress: () =>
+                    this.props.screenProps.functions.checkIn("Wiley")
+                }
+              ]}
+            />
+            <Card
+              footer={[
+                {
+                  text: "Windsor",
+                  onPress: () =>
+                    this.props.screenProps.functions.checkIn("Windsor")
+                }
+              ]}
+            />
+            <Card
+              footer={[
+                {
+                  text: "Earhart",
+                  onPress: () =>
+                    this.props.screenProps.functions.checkIn("Earhart")
+                }
+              ]}
+            />
+          </Card>
         )}
       </Screen>
     );
