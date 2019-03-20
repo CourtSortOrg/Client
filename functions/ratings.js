c2c119a9b1a5ac6c9b9bd3f9eb72324db779bc86
kgaertne <33812693+kgaertne@users.noreply.github.com>
2019-03-19 23:03:21 -0400

    Fixed addRating backend function for dishes

--- functions/ratings.js
+++ functions/ratings.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
-    
+
 var db = admin.firestore();
 // A collection of methods to implement the rating system. Hopefully, a path to cleaner code
 
@@ -14,7 +14,7 @@ module.exports = {
 
         var itemRef = db.collection('Dish').doc(dish);
         var userRef = db.collection('User').doc(userHandle).collection('ItemRatings').doc(dish);
-    
+
         var getItem = await itemRef.get().then(async doc => {
             if (!doc.exists) {
                 console.log("No such dish exists mate!");
@@ -22,26 +22,35 @@ module.exports = {
                 console.log("Adding rating to: " + dish);
                 var itemJSON = await doc.data();
                 await console.log(itemJSON);
-                if("ratings" in itemJSON){
-                    var currScore = itemJSON['totalScore'];
-                    if(currScore == undefined)
-                        currScore = 0;
-                    await itemRef.update({ totalScore: Number(currScore) + Number(rating)});
+                //if("ratings" in itemJSON){
+                var currScore = itemJSON['totalScore'];
+                if(currScore == undefined)
+                    currScore = 0;
+                var currVotes = itemJSON['totalVotes'];
+                if(currVotes == undefined)
+                    currScore = 0;
+                //check if the user has already rated the dish
+                await userRef.get().then(async uDoc => {
+                    if(uDoc.exists){
+                        var oldRating = uDoc.data().rating;
+                        await itemRef.update({ totalScore: Number(currScore) + Number(rating) - Number(oldRating)});
+                    }
+                    else{
+                      await itemRef.update({ totalScore: Number(currScore) + Number(rating)});
 
-                    var currVotes = itemJSON['totalVotes'];
-                    if(currVotes == undefined)
-                        currScore = 0;
-                    await itemRef.update({ totalVotes: currVotes + 1});
-                }
-                else{
-                    itemRef.update({totalScore: rating});
-                    itemRef.update({totalVotes: 1});
-                }
+                      await itemRef.update({ totalVotes: currVotes + 1});
+                    }
+                });
+                //}
+                //else{
+                    //itemRef.update({totalScore: rating});
+                    //itemRef.update({totalVotes: 1});
+                //}
             }
         }).catch(err => {
             throw new Error(err);
         });
-        
+
         await userRef.set({rating: rating, dish: dish});
     }
 }
