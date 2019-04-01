e83b7fc0fe02fa28453435e9c440b6602739735f
Daniel Kambich <dkambich@purdue.edu>
2019-03-31 21:06:04 -0400

    Add meal times to DiningInfo

--- components/main/DiningInfo.js
+++ components/main/DiningInfo.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { FlatList } from "react-native";
 import Screen from "../Nav/Screen";
 import Card from "../components/Card";
 import { Rating } from "react-native-ratings";
 import { MapView } from "expo";
+import Text from "../components/Text";
+import { ListItem } from "react-native-elements";
 const { Marker } = MapView;
 
 export default class DiningInfo extends React.Component {
@@ -41,7 +44,19 @@ export default class DiningInfo extends React.Component {
 
   mealKeyExtractor = meal => meal.name;
 
-  renderMealTime = () => {};
+  renderMealTime = ({ item }) => {
+    console.log(item);
+    return (
+      <ListItem
+        title={item.name}
+        subtitle={`${convertToTwelveHour(
+          item.hours.StartTime
+        )} - ${convertToTwelveHour(item.hours.EndTime)}`}
+        bottomDivider
+        topDivider
+      />
+    );
+  };
 
   render() {
     const { name, location, rating, meals } = this.state;
@@ -57,11 +72,11 @@ export default class DiningInfo extends React.Component {
           <Rating readonly startingValue={rating} />
         </Card>
         <Card header={"Today's Hours"}>
-          {/* <FlatList
+          <FlatList
             keyExtractor={this.mealKeyExtractor}
             data={meals}
-            renderItem={this.renderBlockedUser}
-          /> */}
+            renderItem={this.renderMealTime}
+          />
         </Card>
         <Card header={"Time Spent"} />
         <Card header={"Location"}>
@@ -87,4 +102,12 @@ export default class DiningInfo extends React.Component {
       </Screen>
     );
   }
+}
+
+function convertToTwelveHour(rawTime) {
+  var hour = parseInt(rawTime.substring(0, 2));
+  var suffix = hour >= 12 ? "PM" : "AM";
+  rawTime =
+    ((hour + 11) % 12) + 1 + ":" + rawTime.substring(3, 5) + " " + suffix;
+  return rawTime;
 }
