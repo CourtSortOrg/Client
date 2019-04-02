1aa390fb08b40a9e25411732bb8031afa6788e6a
William Eiffert <weiffert@gmail.com>
2019-04-02 10:24:44 -0400

    Create date and meal picker for recommendations.

--- components/main/Home.js
+++ components/main/Home.js
@@ -1,16 +1,34 @@
 import React from "react";
-import { View, TouchableOpacity } from "react-native";
-import { Button } from "react-native-elements";
+import { View, TouchableOpacity, ScrollView } from "react-native";
+import { Button, Overlay } from "react-native-elements";
 
 import Screen from "../Nav/Screen";
 import CheckInCard from "./CheckInCard";
 import RecommendationsCard from "./RecommendationsCard";
+import ListElement from "../components/ListElement";
 import Card from "../components/Card";
 import Text from "../components/Text";
 
 export default class Home extends React.Component {
+  state = {
+    meal: this.props.screenProps.functions.getNextMeal(),
+    date: this.props.screenProps.functions.getDay(),
+    showDate: false,
+    showMeal: false
+  };
+
+  updateDate = () => {};
+
+  updateMeal = () => {};
+
   render() {
     let diningCourt = ["Hillenbrand", "Wiley", "Windsor", "Ford", "Earhart"];
+    let days = [];
+    let day = new Date();
+
+    for (let i = 0; i < 7; i++) {
+      days.push((day.getDate() + i) % 7);
+    }
 
     return (
       <Screen
@@ -52,40 +70,130 @@ export default class Home extends React.Component {
             screenProps={this.props.screenProps}
           />
         ) : (
-          diningCourt.map((court, index) => (
-            <RecommendationsCard
-              diningCourt={{
-                name: court,
-                dishes: [
-                  {
-                    dish: "Cauliflower",
-                    location: "Ford",
-                    rating: 1.9
-                  },
-                  {
-                    dish: "Creole Jambalaya",
-                    location: "Ford",
-                    rating: 4.3
-                  },
-                  {
-                    dish: "Four Cheese Pizza",
-                    location: "Ford",
-                    rating: 3.2
-                  },
-                  {
-                    dish: "Spicy Red Beans and Rice",
-                    location: "Ford",
-                    rating: 1.8
-                  }
-                ],
-                aggregate: 11.2,
-                total: 4
+          <View>
+            <ListElement Name="Recommendations" type="expandable" />
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-evenly"
               }}
-              navigation={this.props.navigation}
-              screenProps={this.props.screenProps}
-              expand={index == 0}
-            />
-          ))
+            >
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  margin: 16,
+                  padding: 8,
+                  backgroundColor: "#E86515",
+                  borderColor: "black",
+                  borderRadius: 8,
+                  borderWidth: 2
+                }}
+                onPress={() => this.setState({ showDate: true })}
+              >
+                <Text type="bold">{"Date: "}</Text>
+                <Text>
+                  {this.props.screenProps.globals.dayNames[this.state.date]}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  margin: 16,
+                  padding: 8,
+                  backgroundColor: "#E86515",
+                  borderColor: "black",
+                  borderRadius: 8,
+                  borderWidth: 2
+                }}
+                onPress={() => this.setState({ showMeal: true })}
+              >
+                <Text type="bold">{"Meal: "}</Text>
+                <Text>{this.state.meal}</Text>
+              </TouchableOpacity>
+            </View>
+            {diningCourt.map((court, index) => (
+              <RecommendationsCard
+                diningCourt={{
+                  name: court,
+                  dishes: [
+                    {
+                      dish: "Cauliflower",
+                      location: "Ford",
+                      rating: 1.9
+                    },
+                    {
+                      dish: "Creole Jambalaya",
+                      location: "Ford",
+                      rating: 4.3
+                    },
+                    {
+                      dish: "Four Cheese Pizza",
+                      location: "Ford",
+                      rating: 3.2
+                    },
+                    {
+                      dish: "Spicy Red Beans and Rice",
+                      location: "Ford",
+                      rating: 1.8
+                    }
+                  ],
+                  aggregate: 11.2,
+                  total: 4
+                }}
+                navigation={this.props.navigation}
+                screenProps={this.props.screenProps}
+                expand={index == 0}
+              />
+            ))}
+            <Overlay
+              overlayStyle={{ padding: 0 }}
+              containerStyle={{ padding: 0, margin: 0 }}
+              isVisible={this.state.showMeal}
+            >
+              <Card header="Choose a meal" overlay={true}>
+                {this.props.screenProps.globals.mealNames.map(m => (
+                  <Card
+                    footer={[
+                      {
+                        text: m,
+                        onPress: () =>
+                          this.setState({
+                            showMeal: false,
+                            meal: m
+                          })
+                      }
+                    ]}
+                  />
+                ))}
+              </Card>
+            </Overlay>
+            <Overlay
+              overlayStyle={{ padding: 0 }}
+              containerStyle={{ padding: 0, margin: 0 }}
+              isVisible={this.state.showDate}
+            >
+              <Card header="Choose a day" overlay={true}>
+                <ScrollView>
+                {days.map(d => (
+                  <Card
+                    footer={[
+                      {
+                        text: this.props.screenProps.globals.dayNames[d],
+                        onPress: () =>
+                          this.setState({ showDate: false, date: d })
+                      }
+                    ]}
+                  />
+                ))}
+                </ScrollView>
+              </Card>
+            </Overlay>
+          </View>
         )}
       </Screen>
     );
