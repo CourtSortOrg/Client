27ae29c1303feb4fc0a7aa06becf05afb0781d9f
William Eiffert <weiffert@gmail.com>
2019-04-02 21:41:38 -0400

    Remove unnecessary button functionality.

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -113,19 +113,6 @@ export default class Profile extends React.Component {
         screenProps={this.props.screenProps}
         title="Profile"
       >
-        {/* Overlay to let the user change their status */}
-        <Overlay isVisible={this.state.changeStatus} height="30%" width="90%">
-          <View>
-            <Text style={styles.changeStatusText}>Change your status</Text>
-            <ButtonGroup
-              onPress={this.updateStatus}
-              selectedIndex={this.props.screenProps.user.status}
-              buttons={this.props.screenProps.globals.statusMessage}
-              containerStyle={{ height: 60 }}
-            />
-          </View>
-        </Overlay>
-
         {/* Card that shows user information */}
         <Card style={styles.profileInformation}>
           {/* Avatar to show profile picture */}
@@ -152,26 +139,18 @@ export default class Profile extends React.Component {
             size={28}
             style={styles.settingsIcon}
           />
-          {/* Icon that displays the user's status*/}
-          <TouchableOpacity
-            onPress={() => {
-              this.setState({ changeStatus: true });
-            }}
-            style={{
-              backgroundColor: statusColor[this.props.screenProps.user.status],
-              padding: 8,
-              borderRadius: 8
-            }}
-          >
-            <Text>
+          <View style={{ padding: 8 }}>
+            <Text type="bold">
               Status:{" "}
-              {
-                this.props.screenProps.globals.statusMessage[
-                  this.props.screenProps.user.status
-                ]
-              }
+              <Text>
+                {
+                  this.props.screenProps.globals.statusMessage[
+                    this.props.screenProps.user.status
+                  ]
+                }
+              </Text>
             </Text>
-          </TouchableOpacity>
+          </View>
         </Card>
 
         {this.state.restrictions.length > 0 ? (
