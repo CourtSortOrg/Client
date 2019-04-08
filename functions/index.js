7bda0cd177388094b6551782649213307f49256d
kgaertne <33812693+kgaertne@users.noreply.github.com>
2019-04-08 00:19:06 -0400

    DateDishes and DateTimes automatically deleted

--- functions/index.js
+++ functions/index.js
@@ -467,6 +467,23 @@ exports.populateDiningTimes = functions.https.onRequest(async (request, response
       mm = '0' + mm;
     }
     date = yyyy+"-"+mm+"-"+dd;
+
+    //remove old dates
+    var delDate = new Date();
+    delDate.setDate(delDate.getDate() - 2);
+
+    var dd = delDate.getDate();
+    var mm = delDate.getMonth() + 1; //January is 0!
+
+    var yyyy = delDate.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    var delFormat = yyyy+"-"+mm+"-"+dd;
+    await db.collection("DateTimes").doc(delFormat).delete();
   }
 
   const url = "https://api.hfs.purdue.edu/menus/v2/locations/"; // + location + "/" + date;
@@ -714,6 +731,7 @@ exports.individualItemPopulate = functions.https.onRequest(async (request, respo
 exports.populateDishes = functions.https.onRequest(async (request, response)=>{
   var locations = ["hillenbrand", "ford", "wiley", "windsor", "earhart"]
   var date = request.body.date;
+  var userInput = true;
 
   if(date == null){
     var today = new Date();
@@ -729,6 +747,8 @@ exports.populateDishes = functions.https.onRequest(async (request, response)=>{
       mm = '0' + mm;
     }
     date = yyyy+"-"+mm+"-"+dd;
+
+    userInput = false;
   }
 
 
@@ -794,6 +814,26 @@ exports.populateDishes = functions.https.onRequest(async (request, response)=>{
     data.push(await getData(url, locations[i]));
   }
   var updated = await updateDatabase(data);
+
+  if(!userInput){
+    //remove old dates
+    var delDate = new Date();
+    delDate.setDate(delDate.getDate() - 2);
+
+    var dd = delDate.getDate();
+    var mm = delDate.getMonth() + 1; //January is 0!
+
+    var yyyy = delDate.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    var delFormat = yyyy+"-"+mm+"-"+dd;
+    await db.collection("DateDishes").doc(delFormat).delete();
+  }
+
   console.log("done");
   response.send("Finished Population for "+date);
 });
